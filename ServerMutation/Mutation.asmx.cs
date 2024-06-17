5561376da667474c14a0e1769fd531b9929a9bb1
Fercho <160249039+Fernando155244@users.noreply.github.com>
2024-06-17 16:36:33 -0600

    Cambios interrumpidos por actualización

--- ServerMutation/Mutation.asmx.cs
+++ ServerMutation/Mutation.asmx.cs
@@ -20,7 +20,7 @@ namespace ServerMutation
     // [System.Web.Script.Services.ScriptService]
     public class Mutation : System.Web.Services.WebService
     {
-        string conexion = @"server=IP;port=port;uid=User;pwd=Password;database=NombreBaseDatos";
+        string conexion = @"server=mysqludl155244.mysql.database.azure.com;port=3306;uid=Fernando155244;pwd=Luminosc155244;database=cambintest_limpia";
 
         /*Con esta función buscamos saber si tenemos o no conexión*/
         [WebMethod]
@@ -30,7 +30,7 @@ namespace ServerMutation
             return true;
         }
         [WebMethod]
-        public bool Login (int NoSolicitud, String rfc, int Tipo)
+        public bool Login(int NoSolicitud, String rfc, int Tipo)
         {
             //Partes de conexión
             MySqlConnection conn = new MySqlConnection();
@@ -42,17 +42,28 @@ namespace ServerMutation
             conn.ConnectionString = conexion;
             cmd.Connection = conn;
             cmd.CommandType = CommandType.Text;
-            //Mandamos la solicitud de los datos
-            cmd.CommandText = $"select * from Usuarios where Solicitud='{NoSolicitud}' and rfc='{rfc}'and tipo={Tipo}";
+            //Con esto definimos si es de tipo cambios o permutas
+            if(Tipo == 0)
+            {
+                //Mandamos la solicitud de los datos
+                cmd.CommandText = $"SELECT s.id FROM solicitudes_cambios s JOIN trabajador t ON s.id = t.id WHERE t.rfc = '{rfc}' AND s.id = '{NoSolicitud}';";
+            }else if (Tipo == 1)
+            {
+                cmd.CommandText = $"SELECT s.id FROM solicitudes_permuta s JOIN trabajador t ON s.id = t.id WHERE t.rfc = '{rfc}' AND s.id = '{NoSolicitud}';";
+            }
+            
+            
             da.SelectCommand = cmd;
             conn.Open();
             da.Fill(ds);
             conn.Close();
-            if(ds.Tables.Count != 0)
+            if (ds.Tables[0].Rows.Count == 0)
             {
-                return true ;
-            }else { 
-                return false ; 
+                return false;
+            }
+            else
+            {
+                return true;
             }
         }
         [WebMethod]
@@ -68,6 +79,27 @@ namespace ServerMutation
             cmd.Connection = conn;
             cmd.CommandType = CommandType.Text;
             //Mandamos la solicitud de los datos
+            cmd.CommandText = $"SELECT t.paterno, t.materno, t.nombres, s.estado_actual, s.opcion_1,s.opcion_2, s.nivel_educativo, t.rfc FROM trabajador t JOIN solicitudes_cambios s ON s.trabajador = t.id WHERE s.id = '{NoSolicitud}';";
+            da.SelectCommand = cmd;
+            conn.Open();
+            da.Fill(ds);
+            conn.Close();
+            return ds;
+        }
+        [WebMethod]
+        public DataSet Revision(int NoSolicitud)
+        {
+            MySqlConnection conn = new MySqlConnection();
+            MySqlCommand cmd = new MySqlCommand();
+            MySqlDataAdapter da = new MySqlDataAdapter();
+            DataSet ds = new DataSet();
+
+            //Datos de conexion
+            conn.ConnectionString = conexion;
+            cmd.Connection = conn;
+            cmd.CommandType = CommandType.Text;
+            //Mandamos la solicitud de los datos
+            //cmd.CommandText = $"Aqui irá la consulta de la BD";
             cmd.CommandText = $"Aqui irá la consulta de la BD";
             da.SelectCommand = cmd;
             conn.Open();
@@ -76,7 +108,7 @@ namespace ServerMutation
             return ds;
         }
         [WebMethod]
-        public DataSet Revision (int NoSolicitud)
+        public DataSet Graficas(int tipo, bool certificadas, bool canceladas, int status, int referencias)
         {
             MySqlConnection conn = new MySqlConnection();
             MySqlCommand cmd = new MySqlCommand();
@@ -95,27 +127,7 @@ namespace ServerMutation
             conn.Close();
             return ds;
         }
-        [WebMethod]
-        public DataSet Graficas(int tipo, bool certificadas, bool canceladas, int status, int referencias)
-        {
-            MySqlConnection conn = new MySqlConnection();
-        MySqlCommand cmd = new MySqlCommand();
-        MySqlDataAdapter da = new MySqlDataAdapter();
-        DataSet ds = new DataSet();
-
-        //Datos de conexion
-        conn.ConnectionString = conexion;
-            cmd.Connection = conn;
-            cmd.CommandType = CommandType.Text;
-            //Mandamos la solicitud de los datos
-            cmd.CommandText = $"Aqui irá la consulta de la BD";
-            da.SelectCommand = cmd;
-            conn.Open();
-            da.Fill(ds);
-            conn.Close();
-            return ds;
-        }
-        public DataSet Directorio ()
+        public DataSet Directorio()
         {
             MySqlConnection conn = new MySqlConnection();
             MySqlCommand cmd = new MySqlCommand();
